quarkusio/quarkus

Support RunOnVirtualThread annotations on beans implementing a JAX-RS interface

extensions/resteasy-reactive/rest-common/deployment/src/main/java/io/quarkus/resteasy/reactive/common/deployment/JaxrsMethodsProcessor.java
@@ -2,15 +2,20 @@ package io.quarkus.resteasy.reactive.common.deployment;
 
 import java.util.function.Predicate;
 
+import org.jboss.jandex.ClassInfo;
+import org.jboss.jandex.IndexView;
 import org.jboss.jandex.MethodInfo;
+import org.jboss.jandex.Type;
 import org.jboss.resteasy.reactive.common.processor.ResteasyReactiveDotNames;
 
+import io.quarkus.arc.deployment.BeanArchiveIndexBuildItem;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.execannotations.ExecutionModelAnnotationsAllowedBuildItem;
 
 public class JaxrsMethodsProcessor {
     @BuildStep
-    ExecutionModelAnnotationsAllowedBuildItem jaxrsMethods() {
+    ExecutionModelAnnotationsAllowedBuildItem jaxrsMethods(BeanArchiveIndexBuildItem beanArchiveIndex) {
+        IndexView index = beanArchiveIndex.getIndex();
         return new ExecutionModelAnnotationsAllowedBuildItem(new Predicate<MethodInfo>() {
             @Override
             public boolean test(MethodInfo method) {
@@ -19,7 +24,29 @@ public class JaxrsMethodsProcessor {
                 if (method.declaringClass().hasDeclaredAnnotation(ResteasyReactiveDotNames.PATH)) {
                     return true;
                 }
+                if (testMethod(method)) {
+                    return true;
+                }
 
+                // also look at interfaces implemented by the method's declaringClass
+                for (Type interfaceType : method.declaringClass().interfaceTypes()) {
+                    ClassInfo interfaceInfo = index.getClassByName(interfaceType.name());
+                    if (interfaceInfo != null) {
+                        if (interfaceInfo.hasDeclaredAnnotation(ResteasyReactiveDotNames.PATH)) {
+                            return true;
+                        }
+                        MethodInfo overriddenMethodInfo = interfaceInfo.method(method.name(),
+                                method.parameterTypes().toArray(new Type[0]));
+                        if (overriddenMethodInfo != null && testMethod(overriddenMethodInfo)) {
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+
+            private boolean testMethod(MethodInfo method) {
                 // we currently don't handle custom @HttpMethod annotations, should be fine most of the time
                 return method.hasDeclaredAnnotation(ResteasyReactiveDotNames.PATH)
                         || method.hasDeclaredAnnotation(ResteasyReactiveDotNames.GET)

danielbobbert/quarkus-39002

extensions/resteasy-reactive/rest-common/deployment/src/main/java/io/quarkus/resteasy/reactive/common/deployment/JaxrsMethodsProcessor.java
@@ -2,15 +2,20 @@ package io.quarkus.resteasy.reactive.common.deployment;
 
 import java.util.function.Predicate;
 
+import org.jboss.jandex.ClassInfo;
+import org.jboss.jandex.IndexView;
 import org.jboss.jandex.MethodInfo;
+import org.jboss.jandex.Type;
 import org.jboss.resteasy.reactive.common.processor.ResteasyReactiveDotNames;
 
+import io.quarkus.arc.deployment.BeanArchiveIndexBuildItem;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.execannotations.ExecutionModelAnnotationsAllowedBuildItem;
 
 public class JaxrsMethodsProcessor {
     @BuildStep
-    ExecutionModelAnnotationsAllowedBuildItem jaxrsMethods() {
+    ExecutionModelAnnotationsAllowedBuildItem jaxrsMethods(BeanArchiveIndexBuildItem beanArchiveIndex) {
+        IndexView index = beanArchiveIndex.getIndex();
         return new ExecutionModelAnnotationsAllowedBuildItem(new Predicate<MethodInfo>() {
             @Override
             public boolean test(MethodInfo method) {
@@ -19,7 +24,29 @@ public class JaxrsMethodsProcessor {
                 if (method.declaringClass().hasDeclaredAnnotation(ResteasyReactiveDotNames.PATH)) {
                     return true;
                 }
+                if (isJaxrsResourceMethod(method)) {
+                    return true;
+                }
 
+                // also look at interfaces implemented by the method's declaringClass
+                for (Type interfaceType : method.declaringClass().interfaceTypes()) {
+                    ClassInfo interfaceInfo = index.getClassByName(interfaceType.name());
+                    if (interfaceInfo != null) {
+                        if (interfaceInfo.hasDeclaredAnnotation(ResteasyReactiveDotNames.PATH)) {
+                            return true;
+                        }
+                        MethodInfo overriddenMethodInfo = interfaceInfo.method(method.name(),
+                                method.parameterTypes().toArray(new Type[0]));
+                        if (overriddenMethodInfo != null && isJaxrsResourceMethod(overriddenMethodInfo)) {
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+
+            private boolean isJaxrsResourceMethod(MethodInfo method) {
                 // we currently don't handle custom @HttpMethod annotations, should be fine most of the time
                 return method.hasDeclaredAnnotation(ResteasyReactiveDotNames.PATH)
                         || method.hasDeclaredAnnotation(ResteasyReactiveDotNames.GET)